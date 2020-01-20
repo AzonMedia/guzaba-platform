c468e18d091494a4697175ab8052b70bdbbe7d81
Vesko Kenashkov <vesko@azonmedia.com>
2020-01-20 17:50:51 +0200

    fixes

--- app/src/GuzabaPlatform/Platform/Application/Traits/ConnectionConstructorTrait.php
+++ app/src/GuzabaPlatform/Platform/Application/Traits/ConnectionConstructorTrait.php
@@ -11,7 +11,10 @@ trait ConnectionConstructorTrait
     {
         $options = array_filter(self::CONFIG_RUNTIME, fn(string $key) : bool => in_array($key, static::get_supported_options()), ARRAY_FILTER_USE_KEY );
         if ($options['host'] === 'hostname.or.ip') {
-            throw new RunTimeException(sprintf(t::_('It appears that the database connection is not configured. You may try to copy ./registry/local.php.dist to ./registry/local.php and set the needed values there. Or if you are using ./app/bin/start_in_containers or start_containers check ./dockerfiles/GuzabaPlatform/guzaba-platform.env.')), 0 , NULL, 'fb0a71fb-fdf2-4c7e-954a-0208464334b5 ');
+            $message = 'It appears that the database connection %s is not configured.
+You may try to copy ./registry/local.php.dist to ./registry/local.php and set the needed values there.
+Or if you are using ./app/bin/start_in_containers or start_containers check ./dockerfiles/GuzabaPlatform/guzaba-platform.env.';
+            throw new RunTimeException(sprintf(t::_($message), get_class($this)), 0 , NULL, 'fb0a71fb-fdf2-4c7e-954a-0208464334b5');
         }
         parent::__construct($options);
     }
--- app/src/GuzabaPlatform/Platform/Component.php
+++ app/src/GuzabaPlatform/Platform/Component.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 namespace GuzabaPlatform\Platform;
 
 use Guzaba2\Base\Base;
+use GuzabaPlatform\Components\Base\BaseComponent;
 use GuzabaPlatform\Components\Base\Interfaces\ComponentInitializationInterface;
 use GuzabaPlatform\Components\Base\Interfaces\ComponentInterface;
-use GuzabaPlatform\Components\Base\Traits\ComponentTrait;
 
-class Component extends Base implements ComponentInterface, ComponentInitializationInterface
+class Component extends BaseComponent implements ComponentInterface, ComponentInitializationInterface
 {
     protected const CONFIG_DEFAULTS = [
         'services'      => [
@@ -18,9 +18,7 @@ class Component extends Base implements ComponentInterface, ComponentInitializat
 
     protected const CONFIG_RUNTIME = [];
 
-    use ComponentTrait;
-
-    protected const COMPONENT_NAME = "CRUD";
+    protected const COMPONENT_NAME = "Guzaba Platform";
     //https://components.platform.guzaba.org/component/{vendor}/{component}
     protected const COMPONENT_URL = 'https://components.platform.guzaba.org/component/guzaba-platform/guzaba-platform';
     //protected const DEV_COMPONENT_URL//this should come from composer.json
@@ -28,7 +26,7 @@ class Component extends Base implements ComponentInterface, ComponentInitializat
     protected const COMPONENT_VERSION = '0.0.1';//TODO update this to come from the Composer.json file of the component
     protected const VENDOR_NAME = 'Azonmedia';
     protected const VENDOR_URL = 'https://azonmedia.com';
-    protected const ERROR_REFERENCE_URL = 'https://error-reference.guzaba.org/error/';
+    protected const ERROR_REFERENCE_URL = 'https://github.com/AzonMedia/guzaba-platform-docs/tree/master/ErrorReference/';
 
     /**
      * @return array
