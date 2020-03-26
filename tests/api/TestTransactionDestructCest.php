0bfa6abd0f73a7f7cb44c8bfc02d3a0f2fa086ac
Vesko Kenashkov <vesko@azonmedia.com>
2020-03-26 19:35:50 +0200

    added executable script to run all the tests: ./run_tests

--- tests/api/TestTransactionDestructCest.php
+++ tests/api/TestTransactionDestructCest.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+
+use Codeception\Util\HttpCode;
 
 class TestTransactionDestructCest
 {
@@ -10,16 +12,18 @@ class TestTransactionDestructCest
     public function test_destruct(ApiTester $I): void
     {
         $I->sendPUT('/test/transaction/destruct');
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+
+    public function test_nested_destruct(ApiTester $I): void
+    {
+        $I->sendPUT('/test/transaction/nested/destruct');
         $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
     }
 
-//    public function test_nested_destruct(): void
-//    {
-//
-//    }
-//
-//    public function test_multiple_nested_destruct(): void
-//    {
-//
-//    }
+    public function test_multiple_nested_destruct(ApiTester $I): void
+    {
+        $I->sendPUT('/test/transaction/multiple/nested/destruct');
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
+    }
 }
