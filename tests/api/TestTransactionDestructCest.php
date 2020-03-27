2feccfc1c3dfd2e006a5ab18014f981c34764b80
Vesko Kenashkov <vesko@azonmedia.com>
2020-03-27 10:05:19 +0200

    tests moved to separate package

--- tests/api/TestTransactionDestructCest.php
+++ tests/api/TestTransactionDestructCest.php
@@ -1,7 +1,12 @@
 <?php
+declare(strict_types=1);
 
 use Codeception\Util\HttpCode;
 
+/**
+ * Class TestTransactionDestructCest
+ * Runs against @see \GuzabaPlatform\Tests\Controllers\TestCompositeTransactionDestruct
+ */
 class TestTransactionDestructCest
 {
     public function _before(ApiTester $I): void
