950e0f4c0284f8982c27794a3e1c732b36d137d1
Vesko Kenashkov <vesko@azonmedia.com>
2020-03-28 18:47:12 +0200

    added transaction rollback reason tests

--- tests/api/TransactionRollbackReasonCest.php
+++ tests/api/TransactionRollbackReasonCest.php
@@ -3,6 +3,11 @@ declare(strict_types=1);
 
 use Codeception\Util\HttpCode;
 
+/**
+ * Class TransactionRollbackReasonCest
+ *
+ * Tests @see \GuzabaPlatform\Tests\Controllers\TestTransactionRollbackReason
+ */
 class TransactionRollbackReasonCest
 {
     public function test_implicit_rollback_by_conn_ref(ApiTester $I): void
@@ -14,6 +19,24 @@ class TransactionRollbackReasonCest
     public function test_implicit_rollback_by_trans_ref(ApiTester $I): void
     {
         $I->sendPUT('/tests/transaction/rollback/reason/implicit/trans-ref');
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+
+//    public function test_explicit_rollback(ApiTester $I): void
+//    {
+//        $I->sendPUT('/tests/transaction/rollback/reason/implicit/trans-ref');
+//        $I->seeResponseCodeIs(HttpCode::OK);
+//    }
+
+    public function test_base_exception_rollback(ApiTester $I): void
+    {
+        $I->sendPUT('/tests/transaction/rollback/reason/exception/base');
+        $I->seeResponseCodeIs(HttpCode::OK);
+    }
+
+    public function test_exception_rollback(ApiTester $I): void
+    {
+        $I->sendPUT('/tests/transaction/rollback/reason/exception');
         $I->seeResponseCodeIs(HttpCode::OK);
     }
 }
