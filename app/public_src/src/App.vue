fbbf45929c4aa3f315bc453c46d19ae640301cb2
Vesko Kenashkov <vesko@azonmedia.com>
2020-08-15 22:45:44 +0300

    - cleanup in front-end

--- app/public_src/src/App.vue
+++ app/public_src/src/App.vue
@@ -38,6 +38,7 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+*/
 #nav {
   padding: 30px;
 }
--- app/public_src/src/views/Home.vue
+++ app/public_src/src/views/Home.vue
@@ -1,38 +1,42 @@
 <template>
     <div class="home">
         <!-- <LeftNav v-if="isLoggedIn" v-on:loadContent="loadContent" /> -->
+        <!--
         <div style="border: 1px solid red;">
             <LeftNav v-if="isLoggedIn" @loadContent="loadContent" />
         </div>
+        -->
+        <!--
         <div style="border: 1px solid blue;">
             <component :is="dynamicContent" :contentArgs.sync="contentArgs"></component>
         </div>
+        -->
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@GuzabaPlatform.Platform/components/HelloWorld.vue'
-import LeftNav from '@GuzabaPlatform.Platform/components/LeftNav.vue'
-import TreeMenu from '@GuzabaPlatform.Platform/components/TreeMenu.vue'
+//import HelloWorld from '@GuzabaPlatform.Platform/components/HelloWorld.vue'
+//import LeftNav from '@GuzabaPlatform.Platform/components/LeftNav.vue'
+//import TreeMenu from '@GuzabaPlatform.Platform/components/TreeMenu.vue'
 //import Permissions from '@GuzabaPlatform.Platform/views/Permissions.vue'
 //import Crud from '@GuzabaPlatform.Platform/views/Crud.vue'
 
 export default {
     name: 'home',
     components: {
-        HelloWorld,
-        LeftNav,
-        TreeMenu,
+//        HelloWorld,
+//        LeftNav,
+//        TreeMenu,
 //        Permissions,
 //        Crud
     },
     data() {
         return {
-            dynamicContent: "HelloWorld",
-            contentArgs: {
-                msg: "Welcome to Your Vue.js App"
-            }
+            // dynamicContent: "HelloWorld",
+            // contentArgs: {
+            //     msg: "Welcome to Your Vue.js App"
+            // }
         }
     },
     computed : {
@@ -40,11 +44,11 @@ export default {
             return this.$store.getters.isLoggedIn;
         }
     },
-    methods: {
-        loadContent(component, vars) {
-            this.dynamicContent = component;
-            this.contentArgs = vars;
-        }
-    }
+    // methods: {
+    //     loadContent(component, vars) {
+    //         this.dynamicContent = component;
+    //         this.contentArgs = vars;
+    //     }
+    // }
 }
 </script>
