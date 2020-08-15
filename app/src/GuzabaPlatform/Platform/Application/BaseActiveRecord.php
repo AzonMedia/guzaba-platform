c3af0521c05000faea415cd8c34129fa9eacfa36
Vesko Kenashkov <vesko@azonmedia.com>
2020-08-15 22:44:08 +0300

    - docs

--- app/src/GuzabaPlatform/Platform/Application/BaseActiveRecord.php
+++ app/src/GuzabaPlatform/Platform/Application/BaseActiveRecord.php
@@ -1,27 +1,49 @@
 <?php
-declare(strict_types=1);
 
+declare(strict_types=1);
 
 namespace GuzabaPlatform\Platform\Application;
 
-//use Guzaba2\Orm\ActiveRecordWithAuthorization;
 use Guzaba2\Base\Exceptions\RunTimeException;
 use Guzaba2\Orm\ActiveRecord;
 use GuzabaPlatform\Platform\Application\Interfaces\ModelInterface;
 use Guzaba2\Translator\Translator as t;
 
-//abstract class BaseActiveRecord extends ActiveRecordWithAuthorization
+/**
+ * Class BaseActiveRecord
+ * @package GuzabaPlatform\Platform\Application
+ *
+ * Base ActiveRecord class to be inherited by the the ActiveRecord classes of all GuzabaPlatform components
+ */
 abstract class BaseActiveRecord extends ActiveRecord implements ModelInterface
 {
 
+    /**
+     * Returns the object name by using the property provided by self::get_object_name_property()
+     * @return string
+     * @throws RunTimeException
+     * @throws \Azonmedia\Exceptions\InvalidArgumentException
+     * @throws \Guzaba2\Coroutine\Exceptions\ContextDestroyedException
+     * @throws \ReflectionException
+     */
     public function get_object_name() : string
     {
         $object_name_property = static::get_object_name_property();
         return $this->{$object_name_property};
     }
 
+    /**
+     * Returns the property name that should be used as name for the object.
+     * This should be a unique property.
+     * @return string
+     * @throws RunTimeException
+     * @throws \Azonmedia\Exceptions\InvalidArgumentException
+     * @throws \Guzaba2\Coroutine\Exceptions\ContextDestroyedException
+     * @throws \ReflectionException
+     */
     public static function get_object_name_property(): string
     {
+
         if (!isset(static::CONFIG_RUNTIME['object_name_property'])) {
             throw new RunTimeException(sprintf(t::_('The class %1$s is missing the CONFIG_DEFAULTS[\'object_name_property\'] configuation option.'), static::class));
         }
